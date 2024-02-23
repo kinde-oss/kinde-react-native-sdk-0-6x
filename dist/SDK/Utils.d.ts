67e6b4a99d047a25d9e11736b4005c5dd9572124
atifcppprogrammer <54069350+atifcppprogrammer@users.noreply.github.com>
2024-02-23 20:52:50 +0500

    chore: committing `dist` directory for new method

--- dist/SDK/Utils.d.ts
+++ dist/SDK/Utils.d.ts
@@ -10,9 +10,9 @@
  * Do not edit the class manually.
  *
  */
-/// <reference types="react-native" />
 /// <reference types="react-native-inappbrowser-reborn" />
 import * as WebBrowser from 'expo-web-browser';
+import { AuthBrowserOptions } from '../types/Auth';
 import { AdditionalParameters } from '../types/KindeSDK';
 import KindeSDK from './KindeSDK';
 /**
@@ -52,6 +52,6 @@ export declare const checkAdditionalParameters: (additionalParameters?: Addition
  */
 export declare const addAdditionalParameters: (target: Record<string, string | undefined>, additionalParameters?: AdditionalParameters) => Record<string, string | undefined>;
 export declare const isExpoGo: boolean;
-export declare const OpenWebInApp: (url: string, kindeSDK: KindeSDK, options?: WebBrowser.AuthSessionOpenOptions | import("react-native-inappbrowser-reborn").InAppBrowseriOSOptions | import("react-native-inappbrowser-reborn").InAppBrowserAndroidOptions | undefined) => Promise<import("../types/KindeSDK").TokenResponse | null>;
-export declare const openWebBrowser: (url: string, redirectUri: string, options?: WebBrowser.AuthSessionOpenOptions | import("react-native-inappbrowser-reborn").InAppBrowseriOSOptions | import("react-native-inappbrowser-reborn").InAppBrowserAndroidOptions | undefined) => Promise<WebBrowser.WebBrowserRedirectResult | WebBrowser.WebBrowserResult | import("react-native-inappbrowser-reborn").BrowserResult>;
+export declare const OpenWebInApp: (url: string, kindeSDK: KindeSDK, options?: AuthBrowserOptions) => Promise<import("../types/KindeSDK").TokenResponse | null>;
+export declare const openWebBrowser: (url: string, redirectUri: string, options?: AuthBrowserOptions) => Promise<WebBrowser.WebBrowserRedirectResult | WebBrowser.WebBrowserResult | import("react-native-inappbrowser-reborn").BrowserResult>;
 export declare const convertObject2FormData: (obj: Record<string, any>) => FormData;
